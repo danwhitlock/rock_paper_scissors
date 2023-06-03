a3345b6f6cb6074bb9bfc7c93600a0fe38722e3c
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-03 16:58:50 +0100

    Create player choice and run game functions

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
-/* Computer Choice - Randomly return Rock, Paper or Scissors */
+/* Generate Computer Choice - Randomly return Rock, Paper or Scissors */
 
 function getComputerChoice () {
- let choice = Math.floor(Math.random() * 3);
-    if (choice == 0) {
-        return "Rock";
-    } else if (choice == 1) {
-        return "Paper";
-    } else {
-        return "Scissors";
+ let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber == 0) {
+        return "rock";
+    } else if (randomNumber == 1) {
+        return "paper";
+    } else if (randomNumber == 2) {
+        return "scissors";
     };
 } 
 
-console.log(getComputerChoice());
+/* Capture the user's choice 
 
+function playerChoice (userInput) {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+        return userInput;
+    } else {
+        console.log("Invalid Choice");
+    }
+}
+
+*/
+
+/* Run the game */
+
+function playGame (playerChoice, computerChoice) {
+    
+    if (playerChoice === computerChoice) {
+        return "It's a draw"
+
+    } else if  (playerChoice === 'rock') {
+        if (computerChoice === 'scissors') {
+            return 'You win!'
+        } else if (computerChoice === 'paper') {
+            return 'The computer wins!'
+        }
+
+    } else if (playerChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            return 'You win!'
+        } else if (computerChoice === 'scissors') {
+            return 'The computer wins!'
+        } 
+
+    } else if (playerChoice === 'scissors') {
+        if (computerChoice === 'paper') {
+            return 'You win!'
+        } else if (computerChoice === 'rock') {
+            return 'The computer wins!'
+        } 
+    }
+}
+
+
+/* checks */
+playerChoice = 'rock';
+computerChoice = getComputerChoice();
+console.log(`Player chose ${playerChoice}.`);
+console.log(`Computer chose ${computerChoice}.`);
 
