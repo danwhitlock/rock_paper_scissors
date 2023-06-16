e599b0420045b520cb351c9e4eed88fb9df3f6ba
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-06-16 12:18:07 +0100

    Update playRound to catch selections from buttons and track results

--- script.js
+++ script.js
@@ -16,62 +16,145 @@ function getComputerChoice () {
 const rockButton = document.getElementById('rock_btn');
 const paperButton = document.getElementById('paper_btn');
 const scissorsButton = document.getElementById('scissors_btn');
+const resetButton = document.getElementById('reset_btn');
 
 let playerSelection;
 
 rockButton.addEventListener('click', function() {
     playerSelection = 'rock';
+    getComputerChoice ();
+    playRound();
 });
 
 paperButton.addEventListener('click', function() {
     playerSelection = 'paper';
+    getComputerChoice();
+    playRound();
 });
 
 scissorsButton.addEventListener('click', function() {
     playerSelection = 'scissors';
+    getComputerChoice();
+    playRound();
 });
-
-
-/* Function to play one round */
-
-function playRound (playerSelection, computerSelection) {
-    
-    if (playerSelection === computerSelection) {
-        console.log ("It's a draw")
-        
-    } else if  (playerSelection === 'rock') {
-        if (computerSelection === 'scissors') {
-            console.log('Player wins the round!');
-            playerScore++
-        } else if (computerSelection === 'paper') {
-            console.log('The computer wins the round!');
-            computerScore++
-        }
-
-    } else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') {
-            console.log('Player wins the round!');
-            playerScore++
-        } else if (computerSelection === 'scissors') {
-            console.log('The computer wins the round!');
-            computerScore++
-        } 
-
-    } else if (playerSelection === 'scissors') {
-        if (computerSelection === 'paper') {
-            console.log('Player wins the round!');
-            playerScore++
-        } else if (computerSelection === 'rock') {
-            console.log('The computer wins the round!');
-            computerScore++
-        } 
-    }
-}
 
 /* create variables to keep score */
 
 let playerScore = 0;
 let computerScore = 0;
+
+/* Function to play one round */
+
+    const playerChoseDiv = document.querySelector('.player_chose');
+    const computerChoseDiv = document.querySelector('.computer_chose');
+    const outcomeDiv = document.querySelector('.outcome');
+    const playerScoreDiv = document.querySelector('.player_score')
+    const computerScoreDiv = document.querySelector('.computer_score');
+    const ultimateWinnerDiv = document.querySelector('.ultimate_winner');
+    
+
+function playRound () {
+    const computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection) {
+        playerChoseDiv.textContent = `You chose ${playerSelection}`;
+        computerChoseDiv.textContent = `The computer chose ${computerSelection}`;
+        outcomeDiv.textContent = "You both picked the same option.  Nobody scored a point :(";
+                
+    } else if  (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            playerChoseDiv.textContent = `You chose ${playerSelection}`;
+            computerChoseDiv.textContent = `The computer chose ${computerSelection}`;
+            outcomeDiv.textContent = 'You score a point!';
+            playerScore++
+            playerScoreDiv.textContent = `Player score is ${playerScore}`;
+        } else if (computerSelection === 'paper') {
+            playerChoseDiv.textContent = `You chose ${playerSelection}`;
+            computerChoseDiv.textContent = `The computer chose ${computerSelection}`;
+            outcomeDiv.textContent = 'The computer scores a point!';
+            computerScore++
+            computerScoreDiv.textContent = `Computer score is ${computerScore}`;
+        }
+
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            playerChoseDiv.textContent = `You chose ${playerSelection}`;
+            computerChoseDiv.textContent = `The computer chose ${computerSelection}`;
+            outcomeDiv.textContent = 'You score a point!';
+            playerScore++
+            playerScoreDiv.textContent = `Player score is ${playerScore}`;
+        } else if (computerSelection === 'scissors') {
+            playerChoseDiv.textContent = `You chose ${playerSelection}`;
+            computerChoseDiv.textContent = `The computer chose ${computerSelection}`;
+            outcomeDiv.textContent = 'The computer scores a point!';
+            computerScore++
+            computerScoreDiv.textContent = `Computer score is ${computerScore}`;
+        } 
+
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            playerChoseDiv.textContent = `You chose ${playerSelection}`;
+            computerChoseDiv.textContent = `The computer chose ${computerSelection}`;
+            outcomeDiv.textContent = 'You score a point!';
+            playerScore++
+            playerScoreDiv.textContent = `Player score is ${playerScore}`;
+        } else if (computerSelection === 'rock') {
+            playerChoseDiv.textContent = `You chose ${playerSelection}`;
+            computerChoseDiv.textContent = `The computer chose ${computerSelection}`;
+            outcomeDiv.textContent = 'The computer scores a point!';
+            computerScore++
+            computerScoreDiv.textContent = `Computer score is ${computerScore}`;
+        } 
+    }
+
+        if (playerScore == 5 || computerScore == 5) {
+            if (playerScore > computerScore) {
+                ultimateWinnerDiv.textContent = 'Player scored five first and wins the game!';
+                gameFinished();
+            } else {
+                ultimateWinnerDiv.textContent = 'The computer scored five first and wins the game!';
+                gameFinished();
+
+            }
+        }
+}
+
+/* disable the buttons once the game is over */
+
+function gameFinished() {
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+    showResetButton();
+}
+
+function showResetButton() {
+    resetButton.style.display = 'block'
+};
+
+/* Reset the game */
+
+resetButton.addEventListener('click', resetGame);
+
+function resetGame () {
+    playerScore = 0;
+    computerScore = 0;
+    playerChoseDiv.textContent = "";
+    computerChoseDiv.textContent = "";
+    outcomeDiv.textContent = "";
+    playerScoreDiv.textContent = "";
+    computerScoreDiv.textContent = "";
+    ultimateWinnerDiv.textContent = "";
+
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+
+    resetButton.style.display = 'none';    
+}
+
+
+
 
 /* Function to play a five-round game and log the results 
 
